92cc4a88f6412332960cf44e18ae0d8abfe5cbcf
Alexandre Cuva <alexandre.cuva@gmail.com>
2021-04-29 12:14:42 +0200

    Init

---
+++ RapidScrum-API/src/test/java/com/socraagile/scrum/tools/rapidscrum/api/NativeRapidScrumControlerIT.java
@@ -0,0 +1,9 @@
+package com.socraagile.scrum.tools.rapidscrum.api;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeRapidScrumControlerIT extends RapidScrumControlerTest {
+
+    // Execute the same tests but in native mode.
+}
